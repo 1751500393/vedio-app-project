e6c24932f3b4b380d4ca6fe439cf9daa6924d6d0
cenJiaYe <1751500393@qq.com>
2023-05-31 20:58:47 +0800

    20230531 20：58 commit

---
+++ video-app-cate/src/main/java/com/cen/cate/AdminCateApplication.java
@@ -0,0 +1,13 @@
+package com.cen.cate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class AdminCateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminCateApplication.class,args);
+    }
+}
